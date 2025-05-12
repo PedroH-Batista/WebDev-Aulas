761210ec62ce947cd6210ce6373fb0f265a90766
Pedro.B <henriquesilvabatista@outlook.com>
2025-05-11 23:27:19 -0300

    Exercicios 1
    Exercicio1

---
+++ Apostila-1/README.md
@@ -0,0 +1,6 @@
+# WebDevFiap
+Aulas de Web Dev da FIAP
+
+Aplicando conceitos aprendidos em sala de aula
+
+Fazendo questões de ensino de JavaScript
---
+++ Apostila-1/assets/script/script.js
@@ -0,0 +1,64 @@
+// Exercicio 1
+/* let nome = "Pedro";
+let idade = 18;
+
+console.log(`Ola meu nome é ${nome} e tenho ${idade} anos.`);
+ */
+
+//Exercicio 2
+
+/* let valor1 = Number(prompt("digite um valor:"));
+let valor2 = Number(prompt("digite um valor:"));
+console.log(`A soma desse valor é ${valor1 + valor2}`);
+console.log(`A subtração desse valor é ${valor1 - valor2}`);
+console.log(`A multiplicação desse valor é ${valor1 * valor2}`);
+console.log(`A divisão desse valor é ${(valor1 / valor2).toFixed(2)}`);
+ */
+
+// Exercicio 3
+
+/* const tarefas = {
+  Titulo: "Estudar js",
+  Descrição: "Estudando o basico",
+  completed: true,
+};
+
+console.log(`o titulo da sua tarefa é: ${tarefas?.Titulo}`);
+console.log(`a descrição da sua tarefa é: ${tarefas?.Descrição}`);
+console.log(`o status da sua tarefa é: ${tarefas?.completed}`);
+ */
+
+// Exercico 4
+
+/* let numero1 = 10;
+let numero2 = "5";
+console.log(numero1 + Number(numero2));
+ */
+
+// Exercicio 5
+/* const versaoApp = 1.14;
+
+const nome = prompt("Insira o nome do seu app:");
+console.log(`Bem-vindo ao seu aplicativo ${nome} versão ${versaoApp}`);
+ */
+
+// Exercicio 6
+/* let valor1 = Number(prompt("Digite algum valor:"));
+let valor2 = prompt("Digite outro valor:");
+
+console.log(`a == b: ${valor1 == valor2}`);
+console.log(`a === b: ${valor1 === valor2}`);
+console.log(`a > b: ${valor1 > valor2}`);
+ */
+
+//Exercicio 7
+/* let prioridade = 7;
+
+if (prioridade > 5) {
+  prioridade = 5;
+}
+
+console.log(`Sua prioridade original ${prioridade}`);
+console.log(`apos o primeiro incremento ${++prioridade}`);
+console.log(`apos o segundo incremento ${++prioridade}`);
+ */
